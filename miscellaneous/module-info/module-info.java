fe91751a625f18e5ca354a7d0d70c7e3ee0c7f8c
Martin Stam <martin.stam@knmi.nl>
2023-02-06 17:10:50 +0000

    Upload New File

---
+++ miscellaneous/module-info/module-info.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+module Turbowin_jws {
+   requires transitive java.desktop;
+   requires transitive java.logging;
+   requires com.fazecast.jSerialComm;
+   requires jakarta.activation;
+   requires jakarta.mail;
+   
+}
