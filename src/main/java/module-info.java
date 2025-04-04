7af46e8d09ab2224444d8ad573bc8c0e71da9f66
Thomas Maetz <thomas.maetz@code.247bit.de>
2025-04-04 13:15:02 +0200

    Update dependencies for Angus Mail SMTP Provider

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,5 @@
 module turbowin {
     requires transitive java.desktop;
-    requires transitive java.logging;
     requires com.fazecast.jSerialComm;
-    requires jakarta.activation;
-    requires jakarta.mail;
+    requires org.eclipse.angus.mail.smtp;
 }
